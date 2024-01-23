203cb916a94b9e4f075009f8b23a0247d22d670a
Julian Mitton <jdmitton12@gmail.com>
2024-01-23 11:23:08 -0700

    Added Camera Follow Script

---
+++ CameraFollow.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour
+{
+    public Transform targetObject;
+    private Vector3 initialOffset;
+    private Vector3 cameraPosition;
+    
+    
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        initialOffset = transform.position - targetObject.position;
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        cameraPosition = targetObject.position + initialOffset;
+        transform.position = cameraPosition;
+    }
+}
